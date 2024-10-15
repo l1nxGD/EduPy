97d24aa8127c88cee8de8cd8ee0e7336391c39e9
l1nxGD <useer095698@gmail.com>
2024-10-15 17:40:12 +0300

    Update lr1.py

--- lr1.py
+++ lr1.py
@@ -4,7 +4,7 @@ GREEN = '\u001b[42m'
 YELLOW= '\u001b[43m'
 RED = '\u001b[41m'
 END = '\u001b[0m'
-
+#1 задание
 for i in range(8):
     if i < 4:
         print(f'{GREEN}{'    '*3}{YELLOW}{'    '*4}{END}')
@@ -12,8 +12,7 @@ for i in range(8):
         print(f'{GREEN}{'    '*3}{RED}{'    '*4}{END}')
 '''
 #'''
-RED = '\u001b[41m'
-END = '\u001b[0m'
+#2 задание
 #9*5   1 2 3 4 5 6 7 8 9
 print('1 1 + 1 1 1 + 1 1')#1
 print('1 + + + 1 + + + 1')#2
@@ -30,10 +29,8 @@ for i in range(5):
     else:
         print(f'{'   ' * (i - 2)}{RED}{'#  ' * abs(i*2 - 9)}{END}{'   ' * abs(5 - i*2)}{RED}{'#  ' * (9 - i*2)}{END}{'   '*(i-2)}')
     time.sleep(0.5)
-#0 1 2 -> 1 3 4 -> 1 3 7
-#3 4 -> 3 1
-#'''
 
+#3 задание
 plot_list = [[0 for j in range(10)] for i in range(10)]
 res_ls = [i+1 for i in range(10)]
 step = abs(res_ls[0] - res_ls[9])//4
@@ -58,7 +55,7 @@ for i in range(len(plot_list)):
             plot_list[i][j] = '#'
         if plot_list[i][j] == 0:
             plot_list[i][j] = "-"
-#plot_list[9] = (0.0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
 
 for i in plot_list:
     print(*i)
@@ -71,3 +68,19 @@ for i in st:
     if (i >= 0 and i <= 5) or (i <= -5 and i >= -10):
         res.append(i)
 print(sorted(res, reverse=True))
+
+#4 задание
+f = open('sequence.txt')
+st = [float(i) for i in f]
+f.close()
+res1 = list()
+res2 = list()
+for i in st:
+    if (i >= 0 and i <= 5):
+        res1.append(i)
+    elif(i <= -5 and i >= -10):
+        res2.append(i)
+pers1 = int(len(res1)/len(res1) * 100)
+pers2 = int(len(res2)/len(res1) * 100)
+print(f'{RED}{'.' * pers1}{END} | числа от 0 до 5')
+print(f'{GREEN}{'.' * pers2}{END} | числа от -5 до 0')
